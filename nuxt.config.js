1063648790393764608f67a7059f5eff276560e6
Adil Shehzad <7507821-adilshehzad@users.noreply.gitlab.com>
2021-02-12 09:01:20 +0000

    Revert "Adding Vue Theme"
    This reverts commit 2a5279492e22b38ba0a0b8575201871cf2afd0db

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,8 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   docs: {
     primaryColor: '#64748b'
-  },
-  font:
-  {
-      family: 'Roboto',
-
-
   }
 })
 {
-  buildModules: [
-    // Simple usage
-    '@nuxtjs/vuetify',
-
-    // With options
-    ['@nuxtjs/vuetify', { /* module options */ }]
-  ]
+    
 }
