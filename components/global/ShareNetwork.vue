aa906631b0c8eed74994ea31d5f754d9a4f224ce
Adil Shehzad <muhammadadil@ucp.edu.pk>
2021-03-27 14:31:03 +0000

    Adding Share Network Components

---
+++ components/global/ShareNetwork.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" v-model="url">
+    <ShareNetwork
+        network="twitter"
+        :url="url"
+        :title="title"
+        :description="description"
+        :hashtags="hashtags"
+        :twitterUser="twitterUser"
+      >
+      <i class="fab fah fa-lg fa-twitter"></i>
+      <span>Share on Twitter</span>
+    </ShareNetwork>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: 'http://onboarding.grey.software/',
+      title: 'Welcome to the Grey Software Onboarding Process.',
+      description: 'We are delighted to have you join our mission to empower people to create and use open-source software (OSS).',
+      hashtags: 'greysoftware,opensouce,github,gitlab',
+      twitterUser: 'grey_software'
+    }
+  }
+}
+</script>
---
+++ content/en/share-network/share-network.md
@@ -0,0 +1,11 @@
+---
+title: Social Share
+description: ''
+position: 16
+category: Share Network
+
+---
+
+## Share Network
+
+<share-network> </share-network>
