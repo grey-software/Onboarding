8ede6f6d4c7ec1ef19208af846e44f3f1a9e6ff9
Adil Shehzad <muhammadadil@ucp.edu.pk>
2021-03-30 09:42:05 +0000

    Sharing Feature

--- components/global/ShareNetwork.vue
+++ components/global/ShareNetwork.vue
@@ -2,12 +2,10 @@
   <div>
     <input type="text" v-model="url">
     <ShareNetwork
-        network="twitter"
-        :url="url"
-        :title="title"
-        :description="description"
-        :hashtags="hashtags"
-        :twitterUser="twitterUser"
+    
+        v-for="network in networks"
+        :key="network.name"
+        
       >
       <i class="fab fah fa-lg fa-twitter"></i>
       <span>Share on Twitter</span>
@@ -18,12 +16,11 @@
 <script>
 export default {
   data () {
-    return {
-      url: 'http://onboarding.grey.software/',
-      title: 'Welcome to the Grey Software Onboarding Process.',
-      description: 'We are delighted to have you join our mission to empower people to create and use open-source software (OSS).',
-      hashtags: 'greysoftware,opensouce,github,gitlab',
-      twitterUser: 'grey_software'
+    return 
+       
+    {
+      networks:[]
+   
     }
   }
 }
