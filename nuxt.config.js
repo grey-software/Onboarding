91d782e2f3f421375bba2551e2af180914f5eb81
arsalabangash <arsala@grey.software>
2021-08-30 03:41:12 +0500

    updated grey-docs, static images, and settings title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,37 @@ export default theme({
         primaryColor: '#64748b'
     },
     head: {
+        title: 'Onboarding by Grey Software | Democratizing Software Education!',
+        meta: [
+            {
+                name: 'og:title',
+                content: 'Onboarding by Grey Software | Democratizing Software Education!'
+            },
+            {
+                name: 'og:description',
+                content: 'A practical intro to open source software development for new team members or curious explorers!'
+            },
+            {
+                name: 'og:image',
+                content: '/preview.png'
+            },
+            {
+                name: 'twitter:card',
+                content: 'summary_large_image'
+            },
+            {
+                name: 'twitter:title',
+                content: 'Onboarding by Grey Software | Democratizing Software Education!'
+            },
+            {
+                name: 'twitter:description',
+                content: 'A practical intro to open source software development for new team members or curious explorers!'
+            },
+            {
+                name: 'twitter:image',
+                content: '/preview.png'
+            },
+        ],
         script: [
             {
                 src: 'https://plausible.io/js/plausible.js',
